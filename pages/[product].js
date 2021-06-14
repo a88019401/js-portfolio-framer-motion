96bd7d924d436f286a4517f9a86e9b7858b3938c
a88019401 <a88019401@yahoo.com.tw>
2021-06-14 19:11:59 +0800

    2commit

---
+++ pages/[product].js
@@ -0,0 +1,25 @@
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Product() {
+  const {
+    query: { product },
+  } = useRouter();
+  return (
+    <div className={styles.container}>
+      <motion.h1 layoutId="header">{product}</motion.h1>
+      <Link href="/">
+        <div><a>
+          <motion.img
+            layoutId={product}
+            className={styles["big-image"]}
+            src={product + ".PNG"}
+          />
+        </a>
+        </div>
+      </Link>
+    </div>
+  );
+}
